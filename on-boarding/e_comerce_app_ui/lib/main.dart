1629388089e1e31d9a9f867b941b7458784faa51
mamine27 <aminmamine620@gmail.com>
2025-07-24 15:42:36 +0300

    Added Search Page

--- on-boarding/e_comerce_app_ui/lib/main.dart
+++ on-boarding/e_comerce_app_ui/lib/main.dart
@@ -3,104 +3,109 @@ import 'package:flutter/rendering.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    MaterialApp(
+      home: const SearchPage(),
+      theme: ThemeData(
+        textTheme: GoogleFonts.poppinsTextTheme(ThemeData.light().textTheme),
+      ),
+    ),
+  );
+}
+
+Widget singlecard() {
+  return Card(
+    elevation: 4, // shadow depth
+    shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.circular(15), // rounded corners:w
+    ),
+    child: Padding(
+      padding: EdgeInsets.symmetric(
+        horizontal: 0,
+        vertical: 0,
+      ), // inner spacing
+      child: SizedBox(
+        height: 240,
+        width: 366,
+        child: Column(
+          children: [
+            SizedBox(
+              height: 200,
+              width: double.infinity,
+              child: ClipRRect(
+                borderRadius: const BorderRadius.only(
+                  topLeft: Radius.circular(15),
+                  topRight: Radius.circular(15),
+                ),
+                child: Image.asset(
+                  'assets/images/sample.jpeg',
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+
+            SizedBox(height: 20),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: EdgeInsetsGeometry.symmetric(horizontal: 8),
+
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Nike",
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text("120\$"),
+                    ],
+                  ),
+                ),
+
+                Padding(
+                  padding: EdgeInsetsGeometry.symmetric(
+                    horizontal: 8,
+                    vertical: 0,
+                  ),
+
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Men's Shoe",
+                        style: GoogleFonts.poppins(fontWeight: FontWeight.w200),
+                      ),
+                      Row(
+                        children: [
+                          Icon(
+                            Icons.star_rate,
+                            color: const Color.fromARGB(
+                              255,
+                              222,
+                              208,
+                              83,
+                            ), // Set the icon color to yellow
+                          ),
+                          Text("(4.0)"),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
-  Widget singlecard() {
-    return Card(
-      elevation: 4, // shadow depth
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(15), // rounded corners
-      ),
-      child: Padding(
-        padding: EdgeInsets.symmetric(
-          horizontal: 0,
-          vertical: 0,
-        ), // inner spacing
-        child: SizedBox(
-          height: 240,
-          width: 366,
-          child: Column(
-            children: [
-              SizedBox(
-                height: 200,
-                width: double.infinity,
-                child: ClipRRect(
-                  borderRadius: const BorderRadius.only(
-                    topLeft: Radius.circular(15),
-                    topRight: Radius.circular(15),
-                  ),
-                  child: Image.asset(
-                    'assets/images/sample.jpeg',
-                    fit: BoxFit.cover,
-                  ),
-                ),
-              ),
-
-              SizedBox(height: 20),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Padding(
-                    padding: EdgeInsetsGeometry.symmetric(horizontal: 8),
-
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          "Nike",
-                          style: TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        Text("120\$"),
-                      ],
-                    ),
-                  ),
-
-                  Padding(
-                    padding: EdgeInsetsGeometry.symmetric(
-                      horizontal: 8,
-                      vertical: 0,
-                    ),
-
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          "Men's Shoe",
-                          style: GoogleFonts.poppins(
-                            fontWeight: FontWeight.w200,
-                          ),
-                        ),
-                        Row(
-                          children: [
-                            Icon(
-                              Icons.star_rate,
-                              color: const Color.fromARGB(
-                                255,
-                                222,
-                                208,
-                                83,
-                              ), // Set the icon color to yellow
-                            ),
-                            Text("(4.0)"),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
@@ -402,6 +407,84 @@ class _AddPageState extends State<AddPage> {
             ),
           ),
         ),
+      ),
+    );
+  }
+}
+
+class SearchPage extends StatefulWidget {
+  const SearchPage({super.key});
+  @override
+  _SearchPageState createState() => _SearchPageState();
+}
+
+class _SearchPageState extends State<SearchPage> {
+  final _formKey = GlobalKey<FormState>();
+  String _name = '', _price = '', _description = '';
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Search Product',
+          style: GoogleFonts.poppins(fontSize: 16, fontWeight: FontWeight.w500),
+        ),
+        leading: BackButton(),
+      ),
+
+      body: Column(
+        children: [
+          Expanded(
+            child: GridView.count(
+              crossAxisCount: 1, // Number of columns
+              crossAxisSpacing: 10, // Spacing between columns
+              mainAxisSpacing: 10, // Spacing between rows
+              childAspectRatio: 1.5, // Aspect ratio of each item
+              children: List.generate(
+                10, // Number of items
+                (index) => singlecard(),
+              ),
+            ),
+          ),
+
+          Padding(
+            padding: EdgeInsets.all(19),
+            child: Column(
+              children: [
+                Padding(
+                  padding: EdgeInsets.symmetric(vertical: 8),
+                  child: SizedBox(
+                    width: double.infinity,
+                    height: 56,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        if (_formKey.currentState!.validate()) {
+                          _formKey.currentState!.save();
+                          // TODO: handle submit (_name, _price, _description, image, etc)
+                        }
+                      },
+                      style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        backgroundColor: const Color(0xFF3F51F3),
+                      ),
+                      child: Text(
+                        'APPLY',
+                        style: GoogleFonts.poppins(
+                          fontSize: 16,
+                          color: const Color.fromARGB(255, 255, 255, 255),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
