da0b784b3885e9c6f0242a81cdea53361a86b002
mamine27 <aminmamine620@gmail.com>
2025-07-24 13:02:23 +0300

    added AddProduct Page

--- on-boarding/e_comerce_app_ui/lib/main.dart
+++ on-boarding/e_comerce_app_ui/lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/rendering.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const AddPage());
 }
 
 class MyApp extends StatelessWidget {
@@ -193,6 +193,219 @@ class MyApp extends StatelessWidget {
                   ),
                 ),
               ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class AddPage extends StatefulWidget {
+  const AddPage({super.key});
+  @override
+  _AddPageState createState() => _AddPageState();
+}
+
+class _AddPageState extends State<AddPage> {
+  final _formKey = GlobalKey<FormState>();
+  String _name = '', _price = '', _description = '';
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      theme: ThemeData(
+        textTheme: GoogleFonts.poppinsTextTheme(Theme.of(context).textTheme),
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'Add Product',
+            style: GoogleFonts.poppins(
+              fontSize: 16,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+          leading: BackButton(),
+        ),
+
+        body: Padding(
+          padding: EdgeInsets.all(16),
+          child: SingleChildScrollView(
+            child: Form(
+              key: _formKey,
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: 190,
+                    width: double.infinity,
+                    child: GestureDetector(
+                      onTap: () {
+                        // Handle image selection logic here
+                      },
+                      child: Container(
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.grey),
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        child: Center(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Icon(Icons.image, size: 50, color: Colors.grey),
+                              Text(
+                                'Tap to select an image',
+                                style: GoogleFonts.poppins(
+                                  fontSize: 14,
+                                  color: Colors.grey,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+
+                  SizedBox(height: 10),
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text("Name", textAlign: TextAlign.left),
+                  ),
+                  SizedBox(height: 5),
+                  SizedBox(
+                    height: 56,
+                    width: double.infinity,
+                    child: TextFormField(
+                      decoration: InputDecoration(border: OutlineInputBorder()),
+                      onSaved: (v) => _name = v ?? '',
+                      validator: (v) =>
+                          (v == null || v.isEmpty) ? 'Required' : null,
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text("Price", textAlign: TextAlign.left),
+                  ),
+                  SizedBox(height: 5),
+                  SizedBox(
+                    height: 56,
+                    width: double.infinity,
+                    child: TextFormField(
+                      decoration: InputDecoration(border: OutlineInputBorder()),
+                      onSaved: (v) => _name = v ?? '',
+                      validator: (v) =>
+                          (v == null || v.isEmpty) ? 'Required' : null,
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text("Catagory", textAlign: TextAlign.left),
+                  ),
+                  SizedBox(height: 5),
+                  SizedBox(
+                    height: 56,
+                    width: double.infinity,
+                    child: TextFormField(
+                      decoration: InputDecoration(border: OutlineInputBorder()),
+                      onSaved: (v) => _name = v ?? '',
+                      validator: (v) =>
+                          (v == null || v.isEmpty) ? 'Required' : null,
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text("Description", textAlign: TextAlign.left),
+                  ),
+                  SizedBox(height: 5),
+                  SizedBox(
+                    height: 190,
+                    width: double.infinity,
+                    child: TextFormField(
+                      maxLines: 6,
+                      decoration: InputDecoration(border: OutlineInputBorder()),
+                      onSaved: (v) => _description = v ?? '',
+                      validator: (v) =>
+                          (v == null || v.isEmpty) ? 'Required' : null,
+                    ),
+                  ),
+
+                  SizedBox(height: 10),
+                  Padding(
+                    padding: EdgeInsets.all(19),
+                    child: Column(
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.symmetric(vertical: 24),
+                          child: SizedBox(
+                            width: double.infinity,
+                            height: 56,
+                            child: ElevatedButton(
+                              onPressed: () {
+                                if (_formKey.currentState!.validate()) {
+                                  _formKey.currentState!.save();
+                                  // TODO: handle submit (_name, _price, _description, image, etc)
+                                }
+                              },
+                              style: ElevatedButton.styleFrom(
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(8),
+                                ),
+                                backgroundColor: const Color(0xFF3F51F3),
+                              ),
+                              child: Text(
+                                'ADD',
+                                style: GoogleFonts.poppins(
+                                  fontSize: 16,
+                                  color: const Color.fromARGB(
+                                    255,
+                                    255,
+                                    255,
+                                    255,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.symmetric(vertical: 24),
+                          child: SizedBox(
+                            width: double.infinity,
+                            height: 56,
+                            child: ElevatedButton(
+                              onPressed: () {
+                                if (_formKey.currentState!.validate()) {
+                                  _formKey.currentState!.save();
+                                  // TODO: handle submit (_name, _price, _description, image, etc)
+                                }
+                              },
+                              style: ElevatedButton.styleFrom(
+                                side: BorderSide(color: Colors.red, width: 1),
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(8),
+                                ),
+                                backgroundColor: Colors.white,
+                              ),
+                              child: Text(
+                                'Delete',
+                                style: GoogleFonts.poppins(
+                                  fontSize: 16,
+                                  color: const Color.fromARGB(255, 247, 0, 0),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
