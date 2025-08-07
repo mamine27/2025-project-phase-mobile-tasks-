9fa83aaae2681eb598618ce0212f0fb43ff1df79
mamine27 <aminmamine620@gmail.com>
2025-08-07 23:56:19 +0300

    feat: Refactor SignIn page to use StatefulWidget and implement form validation

--- on-boarding/e_comerce_app_ui/lib/features/auth/presentations/pages/Sign_in.dart
+++ on-boarding/e_comerce_app_ui/lib/features/auth/presentations/pages/Sign_in.dart
@@ -1,150 +1,189 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class SignIn extends StatelessWidget {
+import '../../../../injection_container.dart';
+import '../../data/datasources/auth_remote_datasource_impl.dart';
+
+class SignIn extends StatefulWidget {
   const SignIn({super.key});
 
   static const routeName = '/signin';
 
   @override
+  State<SignIn> createState() => _SignInState();
+}
+
+class _SignInState extends State<SignIn> {
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  Future<void> _submit() async {
+    if (_formKey.currentState!.validate()) {
+      // All validations passed, proceed with sign up logic
+      final authremote = sl<AuthRemoteDataSourceImpl>();
+      ScaffoldMessenger.of(
+        context,
+      ).showSnackBar(const SnackBar(content: Text('Signing in...')));
+      final result = await authremote.signIn(
+        _emailController.text.trim(),
+        _passwordController.text.trim(),
+      );
+
+      result.fold(
+        (failure) {
+          // Show failure message
+          print("shit");
+          ScaffoldMessenger.of(
+            context,
+          ).showSnackBar(SnackBar(content: Text(failure.message)));
+        },
+        (_) {
+          // Show success and navigate
+          print("w");
+          ScaffoldMessenger.of(
+            context,
+          ).showSnackBar(const SnackBar(content: Text('Sign in successful!')));
+          Navigator.pushReplacementNamed(context, '/');
+        },
+      );
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          const SizedBox(height: 150),
-          Center(
-            child: Container(
-              width: 144,
-              height: 50,
-              decoration: BoxDecoration(
-                borderRadius: const BorderRadius.all(Radius.circular(6.41)),
-                color: Colors.white,
-                border: Border.all(color: Colors.grey),
-                boxShadow: const [
-                  BoxShadow(
-                    color: Colors.black26,
-                    offset: Offset(0, 4),
-                    blurRadius: 4,
-                  ),
-                ],
-              ),
-              child: Center(
-                child: Text(
-                  'ECOM',
-                  style: GoogleFonts.caveatBrush(
-                    color: const Color(0xff3F51F3),
-                    fontSize: 35,
-                    fontWeight: FontWeight.w500,
-                  ),
-                ),
-              ),
-            ),
-          ),
-
-          const SizedBox(height: 40),
-          Center(
-            child: Text(
-              'Sign into your account',
-              style: GoogleFonts.poppins(
-                wordSpacing: 2,
-                fontSize: 26.72,
-                fontWeight: FontWeight.w600,
-              ),
-            ),
-          ),
-
-          const SizedBox(height: 20),
-          Padding(
-            padding: const EdgeInsetsGeometry.symmetric(horizontal: 40),
-            child: Column(
-              children: [
-                const SizedBox(height: 10),
-                const Align(
-                  alignment: Alignment.centerLeft,
-                  child: Text('Email', textAlign: TextAlign.left),
-                ),
-                const SizedBox(height: 5),
-                SizedBox(
-                  height: 56,
-                  width: double.infinity,
-                  child: TextFormField(
-                    decoration: const InputDecoration(
-                      hintText: 'ex: jon.smith@email.com',
-                      hintStyle: TextStyle(color: Color(0xff888888)),
-                      border: OutlineInputBorder(),
-                    ),
-                    validator: (v) =>
-                        (v == null || v.isEmpty) ? 'Required' : null,
-                  ),
-                ),
-                const SizedBox(height: 10),
-                const Align(
-                  alignment: Alignment.centerLeft,
-                  child: Text('Password', textAlign: TextAlign.left),
-                ),
-                const SizedBox(height: 5),
-                SizedBox(
-                  height: 56,
-                  width: double.infinity,
-                  child: TextFormField(
-                    decoration: const InputDecoration(
-                      hintText: '********',
-                      hintStyle: TextStyle(color: Color(0xff888888)),
-                      border: OutlineInputBorder(),
-                    ),
-                    validator: (v) =>
-                        (v == null || v.isEmpty) ? 'Required' : null,
-                  ),
-                ),
-                const SizedBox(height: 10),
-                SizedBox(
-                  width: double.infinity,
-                  height: 56,
-                  child: ElevatedButton(
-                    onPressed: () {},
-                    style: ElevatedButton.styleFrom(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(8),
+      body: SingleChildScrollView(
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              const SizedBox(height: 150),
+              Center(
+                child: Container(
+                  width: 144,
+                  height: 50,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(6.41),
+                    color: Colors.white,
+                    border: Border.all(color: Colors.grey),
+                    boxShadow: const [
+                      BoxShadow(
+                        color: Colors.black26,
+                        offset: Offset(0, 4),
+                        blurRadius: 4,
                       ),
-                      backgroundColor: const Color(0xFF3F51F3),
-                    ),
+                    ],
+                  ),
+                  child: Center(
                     child: Text(
-                      'SIGN IN',
-                      style: GoogleFonts.poppins(
-                        fontSize: 16,
-                        color: const Color.fromARGB(255, 255, 255, 255),
+                      'ECOM',
+                      style: GoogleFonts.caveatBrush(
+                        color: const Color(0xff3F51F3),
+                        fontSize: 35,
+                        fontWeight: FontWeight.w500,
                       ),
                     ),
                   ),
                 ),
-                const SizedBox(height: 50),
-                Align(
-                  alignment: Alignment.center,
-                  child: SizedBox(
-                    child: Center(
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          const Text('Don’t have an account?'),
-                          const SizedBox(width: 5),
-                          GestureDetector(
-                            onTap: () {
-                              Navigator.pushNamed(context, '/signup');
-                            },
-                            child: const Text(
-                              'SIGN UP',
-                              style: TextStyle(color: Color(0xff3F51F3)),
-                            ),
+              ),
+              const SizedBox(height: 40),
+              Text(
+                'Sign into your account',
+                style: GoogleFonts.poppins(
+                  wordSpacing: 2,
+                  fontSize: 26.72,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              const SizedBox(height: 20),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 40),
+                child: Column(
+                  children: [
+                    const Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text('Email'),
+                    ),
+                    const SizedBox(height: 5),
+                    SizedBox(
+                      height: 56,
+                      child: TextFormField(
+                        controller: _emailController,
+                        decoration: const InputDecoration(
+                          hintText: 'ex: jon.smith@email.com',
+                          hintStyle: TextStyle(color: Color(0xff888888)),
+                          border: OutlineInputBorder(),
+                        ),
+                        validator: (v) =>
+                            (v == null || v.isEmpty) ? 'Required' : null,
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    const Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text('Password'),
+                    ),
+                    const SizedBox(height: 5),
+                    SizedBox(
+                      height: 56,
+                      child: TextFormField(
+                        controller: _passwordController,
+                        obscureText: true,
+                        decoration: const InputDecoration(
+                          hintText: '********',
+                          hintStyle: TextStyle(color: Color(0xff888888)),
+                          border: OutlineInputBorder(),
+                        ),
+                        validator: (v) =>
+                            (v == null || v.isEmpty) ? 'Required' : null,
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    SizedBox(
+                      width: double.infinity,
+                      height: 56,
+                      child: ElevatedButton(
+                        onPressed: () => _submit(),
+                        style: ElevatedButton.styleFrom(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8),
                           ),
-                        ],
+                          backgroundColor: const Color(0xFF3F51F3),
+                        ),
+                        child: Text(
+                          'SIGN IN',
+                          style: GoogleFonts.poppins(
+                            fontSize: 16,
+                            color: Colors.white,
+                          ),
+                        ),
                       ),
                     ),
-                  ),
+                    const SizedBox(height: 50),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Text('Don’t have an account?'),
+                        const SizedBox(width: 5),
+                        GestureDetector(
+                          onTap: () {
+                            Navigator.pushNamed(context, '/signup');
+                          },
+                          child: const Text(
+                            'SIGN UP',
+                            style: TextStyle(color: Color(0xff3F51F3)),
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 30),
+                  ],
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
