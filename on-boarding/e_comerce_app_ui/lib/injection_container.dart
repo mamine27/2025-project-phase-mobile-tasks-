c7d1e02230e79f556b60198da9945522e8dfde29
mamine27 <aminmamine620@gmail.com>
2025-08-08 09:48:42 +0300

    refactor: Remove commented instruction in setupLocator for clarity

--- on-boarding/e_comerce_app_ui/lib/injection_container.dart
+++ on-boarding/e_comerce_app_ui/lib/injection_container.dart
@@ -4,7 +4,6 @@ import 'features/auth/data/datasources/auth_remote_datasource_impl.dart';
 final sl = GetIt.instance; // sl = service locator
 
 void setupLocator(authclient, authlocal) {
-  // Register your dependencies
   sl.registerLazySingleton<AuthRemoteDataSourceImpl>(
     () => AuthRemoteDataSourceImpl(authclient, authlocal),
   );
